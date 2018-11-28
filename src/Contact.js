d39c5a5f497842e4d1dbacf3e4b342c82cc9ae30
Selvin Norman <selvin715@gmail.com>
2018-11-27 16:41:56 -0800

    css contact page

--- src/Contact.js
+++ src/Contact.js
@@ -6,14 +6,14 @@ export default function Contact() {
     <React.Fragment>
       <Nav />
       <main>   
-        <section>
-          <article>
+        <section className="contact-section">
+          <article className="contact-article">
             <h2 className="contactMe">Contact Me @</h2>
             <div class="contact">
               <ul className="contacts">
-                <li><a href="https://github.com/selvinor"><img src={require('./img/github.png')} alt="Contact me via github"/>github.com/selvinor</a></li>
-                <li><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/linkedin-logo.png')} alt="Contact me via linkedin"/>www.linkedin.com/in/selnorman</a></li>
-                <li><a href="mailto:selvin715@gmail.com"><img src={require('./img/email.png')} alt="Contact me via email"/>Contact me via email</a></li>
+                <li><a href="https://github.com/selvinor"><img src={require('./img/github.png')} alt="Contact me via github"/><br />github.com/selvinor</a></li>
+                <li><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/linkedin-logo.png')} alt="Contact me via linkedin"/><br />www.linkedin.com/in/selnorman</a></li>
+                <li><a href="mailto:selvin715@gmail.com"><img src={require('./img/email.png')} alt="Contact me via email"/><br />Contact me via email</a></li>
               </ul>
             </div>
           </article> 
