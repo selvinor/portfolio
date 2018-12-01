5dc49103f51acc1873e4489a681b0fccf137c6d5
Selvin Norman <selvin715@gmail.com>
2018-11-30 23:56:29 -0800

    updated home images for media queries

--- src/Home.js
+++ src/Home.js
@@ -21,7 +21,7 @@ export default class Home extends Component {
           <ul className="home-contacts">
             <li className="home-contact home-contact1"><a href="https://github.com/selvinor"><img src={require('./img/github.png')} alt="Contact me via github"/></a></li>
             <li className="home-contact home-contact2"><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/linkedin-logo.png')} alt="Contact me via linkedin"/></a></li>
-            <li className="home-contact home-contact3 email"><a href="mailto:selvin715@gmail.com"><img src={require('./img/email.png')} alt="Email me"/></a></li>
+            <li className="home-contact home-contact3 email"><a href="mailto:selnorman.developer@gmail.com"><img src={require('./img/email.png')} alt="Email me"/></a></li>
           </ul> 
          </div>
   
