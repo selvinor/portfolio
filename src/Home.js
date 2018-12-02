9472505f7b13c628fed5d967aac244607aa82099
Selvin Norman <selvin715@gmail.com>
2018-12-01 16:24:17 -0800

    adjusting flex for about page

--- src/Home.js
+++ src/Home.js
@@ -16,14 +16,15 @@ export default class Home extends Component {
               <p>is a Full Stack Developer looking to create mobile-first software for entrepreneurs and businesses.</p>
             </article> 
           </section>
-        </main>   
-        <div className="links">
+          <div className="links">
           <ul className="home-contacts">
             <li className="home-contact home-contact1"><a href="https://github.com/selvinor"><img src={require('./img/github.png')} alt="Contact me via github"/></a></li>
             <li className="home-contact home-contact2"><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/linkedin-logo.png')} alt="Contact me via linkedin"/></a></li>
             <li className="home-contact home-contact3 email"><a href="mailto:selnorman.developer@gmail.com"><img src={require('./img/email.png')} alt="Email me"/></a></li>
           </ul> 
          </div>
+        </main>   
+
   
       </React.Fragment>  
     );  
