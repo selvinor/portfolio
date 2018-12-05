5b75b19a6ff6716178c7ea0447089bd2cfefb806
Selvin Norman <selvin715@gmail.com>
2018-12-05 09:51:11 -0800

    revised about page text

--- src/About.js
+++ src/About.js
@@ -17,9 +17,10 @@ export default class About extends Component {
               <div className="avatar">
                 <img src={require('./img/me_160_circle.png')} alt=""/>
               </div>
-              <p className="about-text">My passion is fueled by a mix of technology and art. As a full stack developer, I enjoy creating products and experiences that leverage my skills as a photographer, electronic musician and graphic artist.</p>
-              <p className="about-text">My work experience ranges from small business ownership to software development and support, to deploying network and server infrastructure across the globe.</p>
-              <p className="about-text">Delivering value to my team and project is my primary focus.</p>             
+              <p className="about-text"> As a full stack developer, I enjoy creating apps that make it easy for businesses and customers to interact with products and services.  </p>
+              <p className="about-text">I am passionate about delivering value to my team and project. My passion is fueled by a mix of skills as a developer, photographer, electronic musician and graphic artist.</p>
+              <p className="about-text">My experience ranges from local small business ownership, to financial institution software development and support, to deploying network and server infrastructure across the globe.</p>
+          
             </article> 
           </section>
         </main>    
