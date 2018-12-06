7f0fe91ae6408f79503c4638c193d2b49d177ace
Selvin Norman <selvin715@gmail.com>
2018-12-06 13:07:13 -0800

    adjusted fonts, text colors

--- src/Contact.js
+++ src/Contact.js
@@ -16,9 +16,9 @@ export default class Contact extends Component {
                 <input type="email" maxLength="50" placeholder="Title..." />
                 <textarea rows="8" maxLength="500" placeholder="Contents..." />               
               </div>
-              <button><a href="mailto:selnorman.developer@gmail.com?subject=&amp;body=">Send E-mail</a></button>
               <ul className="contacts">
                 <li><a href="https://github.com/selvinor"><img src={require('./img/github.png')} alt="Contact me via github"/></a></li>
+                <li><button><a href="mailto:selnorman.developer@gmail.com?subject=&amp;body=">Send E-mail</a></button></li>
                 <li><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/linkedin-logo.png')} alt="Contact me via linkedin"/></a></li>
               </ul>
             </article> 
