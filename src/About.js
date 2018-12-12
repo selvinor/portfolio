00339200fc7f4169f6cdf6eb46f6d9f65872c25a
Selvin Norman <selvin715@gmail.com>
2018-12-11 23:08:39 -0800

    updated about

--- src/About.js
+++ src/About.js
@@ -16,11 +16,9 @@ export default class About extends Component {
                 <img src={require('./img/me_160_circle.png')} alt=""/>
               </div>
               <div className="about-desc">
-                <p className="about-text">Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities. I'm an avid electronic musician, photographer and gearhead. </p>
-                <p>I spent the last couple of years as the co-owner of a small flower shop in downtown Portland, Oregon. My struggles with existing floral industry software and vendors influenced me to want to develop my own software for the business, so the next step was to go for intensive training to update my skills.</p>
-                <p>Prior to becoming a small business owner, I worked in the videogame industry, designing and deploying server and network infrastructure on a global scale. After several years of climbing up and down ladders and doing all the other related physical activities, software development started looking more and more attractive.</p>
-                <p>Before switching to hardware, I traveled extensively as a banking software consultant, but after 10 years, left to focus more on family.</p>
-                <p>It's very exciting to be starting a new career as a developer. I'm looking forward to joining a team that values collaboration and fosters personal and professional growth.</p>
+                <p className="about-text">Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities.</p>
+                <p>I've spent the last couple of years as a co-owner of a small flower shop in downtown Portland, Oregon. In my spare time, I play live music around town with an array of synthesizers.  I'm an avid electronic musician, photographer and gearhead. </p>
+                <p>I'm looking forward to joining a team where I can collaborate with awesome people and continue to grow.</p>
               </div>
             </article> 
           </section>
