a8c40b8f14d949e6ce88be01dc7848ce95d81ccc
Selvin Norman <selvin715@gmail.com>
2018-12-11 23:39:30 -0800

    updated contact css

--- src/About.js
+++ src/About.js
@@ -17,7 +17,7 @@ export default class About extends Component {
               </div>
               <div className="about-desc">
                 <p className="about-text">Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities.</p>
-                <p>I've spent the last couple of years as a co-owner of a small flower shop in downtown Portland, Oregon. In my spare time, I play live music around town with an array of synthesizers.  I'm an avid electronic musician, photographer and gearhead. </p>
+                <p>I've spent the last couple of years as a co-owner of a small flower shop in downtown Portland, Oregon. In my spare time, you can find me playing live music around town with an array of synthesizers.  I'm an avid electronic musician, photographer and gearhead. </p>
                 <p>I'm looking forward to joining a team where I can collaborate with awesome people and continue to grow.</p>
               </div>
             </article> 
