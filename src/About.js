78b760f12648f6d6cc455fca7cd3a65adf588a84
Selvin Norman <selvin715@gmail.com>
2018-12-11 19:07:25 -0800

    updated about page text

--- src/About.js
+++ src/About.js
@@ -17,8 +17,10 @@ export default class About extends Component {
               </div>
               <div className="about-desc">
                 <p className="about-text">Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities. I'm an avid electronic musician, photographer and gearhead. </p>
-                <p>Prior to becoming a full stack developer, I spent five years designing and deploying server and network infrastructure on a global scale. After several years of climbing up and down ladders and doing all the other related physical activities, software development started looking more and more attractive. After 10 years traveling across the US and Mexico as a consultant working on banking applications, I left to focus more on family. </p>
-                <p>Fast-forward to now and it's very exciting to reincarnate as a developer in the Internet age. I'm looking forward to joining a team that values collaboration and fosters further professional growth.</p>
+                <p>I spent the last couple of years as the co-owner of a small flower shop in downtown Portland, Oregon. My struggles with existing floral industry software and vendors influenced me to want to develop my own software for the business, so the next step was to go for intensive training to update my skills.</p>
+                <p>Prior to becoming a small business owner, I worked in the videogame industry, designing and deploying server and network infrastructure on a global scale. After several years of climbing up and down ladders and doing all the other related physical activities, software development started looking more and more attractive.</p>
+                <p>Before switching to hardware, I traveled extensively as a banking software consultant, but after 10 years, left to focus more on family.</p>
+                <p>It's very exciting to be starting a new career as a developer. I'm looking forward to joining a team that values collaboration and fosters personal and professional growth.</p>
               </div>
             </article> 
           </section>
