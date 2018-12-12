f2b054ab9c126d9cc920464a87f2e1425b562402
Selvin Norman <selvin715@gmail.com>
2018-12-11 18:03:08 -0800

    updated about page

--- src/About.js
+++ src/About.js
@@ -16,9 +16,9 @@ export default class About extends Component {
                 <img src={require('./img/me_160_circle.png')} alt=""/>
               </div>
               <div className="about-desc">
-                <p className="about-text"> As a full stack developer, I enjoy creating apps that make it easy for businesses and customers to interact with products and services.  </p>
-                <p className="about-text">I am passionate about delivering value to my team and project. My passion is fueled by developing new skills as a developer, photographer, electronic musician and graphic artist.</p>
-                <p className="about-text">My experience ranges from local small business ownership, to financial institution software development and support, to deploying network and server infrastructure across the globe.</p>
+                <p className="about-text"> Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities. I'm an avid electronic musician, photographer and gamer.</p>
+                <p>Prior to becoming a full stack developer, I spent five years designing and deploying server and network infrastructure on a global scale. After several years of climbing up and down ladders and doing all the other related physical activities, software development started looking more and more attractive. Prior to my years working with hardware, I had a ten year career as a software consultant working on mainframe banking systems across the US and in Mexico.</p>
+                <p>It's very exciting to reincarnate myself as a developer in the Internet age. I'm looking forward to joining a team and contributing with my newly-minted skills and broad experience.</p>
               </div>
             </article> 
           </section>
