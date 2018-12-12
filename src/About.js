cec12e92fb906489de5b3aae5836e4a4f96131cd
Selvin Norman <selvin715@gmail.com>
2018-12-11 18:38:16 -0800

    updated about page

--- src/About.js
+++ src/About.js
@@ -16,9 +16,9 @@ export default class About extends Component {
                 <img src={require('./img/me_160_circle.png')} alt=""/>
               </div>
               <div className="about-desc">
-                <p className="about-text"> Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities. I'm an avid electronic musician, photographer and gamer.</p>
-                <p>Prior to becoming a full stack developer, I spent five years designing and deploying server and network infrastructure on a global scale. After several years of climbing up and down ladders and doing all the other related physical activities, software development started looking more and more attractive. Prior to my years working with hardware, I had a ten year career as a software consultant working on mainframe banking systems across the US and in Mexico.</p>
-                <p>It's very exciting to reincarnate myself as a developer in the Internet age. I'm looking forward to joining a team and contributing with my newly-minted skills and broad experience.</p>
+                <p className="about-text">Hello, I'm a full stack developer based in Portland Oregon.  My passion has always been tinkering with machines, gadgets, and software - figuring out how they work, why they broke, and how to give them new abilities. I'm an avid electronic musician, photographer and gearhead. </p>
+                <p>Prior to becoming a full stack developer, I spent five years designing and deploying server and network infrastructure on a global scale. After several years of climbing up and down ladders and doing all the other related physical activities, software development started looking more and more attractive. After 10 years traveling across the US and Mexico as a consultant working on banking applications, I left to focus more on family. </p>
+                <p>Fast-forward to now and it's very exciting to reincarnate as a developer in the Internet age. I'm looking forward to joining a team that values collaboration and fosters further professional growth.</p>
               </div>
             </article> 
           </section>
