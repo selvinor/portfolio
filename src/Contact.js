e5ba7cdef9ca78a6e2d66df4725429ad66578abd
Selvin Norman <selvin715@gmail.com>
2018-12-11 16:00:12 -0800

    css changes to contact

--- src/Contact.js
+++ src/Contact.js
@@ -11,15 +11,15 @@ export default class Contact extends Component {
         <main id="contact-main">   
           <section id="contact-section">
             <article id="contact-article">
-              <h3 className="contactMe">Contact Me @</h3>
+              <h3 className="contactMe">Contact Me</h3>
               <div className="contact-form">
                 <input type="email" maxLength="50" placeholder="Title..." />
                 <textarea rows="8" maxLength="500" placeholder="Contents..." />               
               </div>
               <ul className="contacts">
-                <li><a href="https://github.com/selvinor"><img src={require('./img/github.png')} alt="Contact me via github"/></a></li>
+                <li><a href="https://github.com/selvinor"><img src={require('./img/GitHub-Mark-120px-plus.png')} alt="Contact me via github"/></a></li>
                 <li><button><a href="mailto:selnorman.developer@gmail.com?subject=&amp;body=">Send E-mail</a></button></li>
-                <li><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/linkedin-logo.png')} alt="Contact me via linkedin"/></a></li>
+                <li><a href="https://www.linkedin.com/in/selvin-norman-6ba4aa2b/"><img src={require('./img/icons8-linkedin-100.png')} alt="Contact me via linkedin"/></a></li>
               </ul>
             </article> 
           </section>
